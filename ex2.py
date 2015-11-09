9671a61b7bf2325115270b110dca18ef08171610
gsedubun <gsedubun@gmail.com>
2015-11-09 13:49:57 +0700

    add existing examples

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# a comment, ini comment & docs
+#testing docs
+
+##print("test print data di python 3.4") # ini comment
+print("multiple lines")
+##
+
+#comment lagi
+
+print('#test print lagi setelah comment') 
---
+++ ex9.py
@@ -0,0 +1,16 @@
+# Here's soome new strange stuff
+
+days = "Mon Tue Web Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:",days)
+print("Here are the months:",months)
+
+print("""
+thers's something going on here.
+with the three double-quotes.
+we'll be able to type as much as we like.
+even 4 lines if we want, or 5, or 6.
+""")
+
+
